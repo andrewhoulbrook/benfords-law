072caff6a5ab3436c6a3a719180de94d5bdcfcf3
Baba Hao <47100671+ba2baha3o@users.noreply.github.com>
2019-05-17 18:00:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,135 @@
+# Benford's Law
+
+A script to compare a given dataset to Benford's Law and its generalized form.
+
+## Getting Started
+
+This script is written largely as the product of my learning about and experimenting with Benford's Law, especially its application to fraud detection tasks.  
+
+I'd strongly recommend checking out [Marcel Milcent's](https://github.com/milcent/benford_py/blob/master/Demo.ipynb) excellent Python package, which is a far more professional offering than my humble effort. Marcel's work is also now available as a PyPi Package!  
+
+### Benford's Law
+
+Benford's Law, also known as the Law of First Digits, is the finding that for certain types of datasets, the first digits (or numerals) of numbers contained within a series do not display a uniform distribution, but rather follow a distribution shown below:
+
+![Benford Distribution](/doc/benford_dist.png)
+
+A more detailed overview of Benford's Law can be [found here](http://mathworld.wolfram.com/BenfordsLaw.html) and [here](http://www.nigrini.com/ForensicAnalytics.htm).
+
+Benford's Law is known to apply across a diverse range of datasets, for example: census data, street addresses, stock prices, house prices, population data, death rates, lengths of rivers, areas of land masses and lottery numbers. 
+
+In general, Benford's Law usually holds when a series of numerical data has some (or most) of the following characteristics:
+
+* Data values have varying degrees of magnitude
+* Data values formed through mathematical combination of numbers from several distributions 
+* Data with no pre-determined minimum or maximum limits
+* A relatively large number of records 
+* Data doesn't contain identifier/index-type numbers (e.g. SSINs, account numbers, phone numbers etc...) 
+* Data is right-skewed, mean is less than the median
+
+Data that conforms to the above characteristics is common in accounting which has lead to Benford's Law being used as a heuristic tool to detect the potential of fraud. 
+
+### Breaking the Law - Fraud Detection
+
+Benford's Law can also be generalised beyond the first digit to consider probability distributions for the first two and first three digits, for example. Distribution of the last two digits has also been suggested for identifying artificially rounded or fabricated data and flag data that could warrant further investigation. 
+
+[This report](https://www.acfe.com/uploadedFiles/Shared_Content/Products/Self-Study_CPE/Using%20Benford's%20Law_2018_final_extract.pdf) by the Association of Certified Fraud Examiner's (ACFE) provides much more background on the application of Benford's Law to fraud detection and generalising Benford's Law to include analysis of second and third digit distributions. 
+
+The first digit distribution alone doesn't help narrow the data into a subset of potential fraudulent data. It also doesn't necessarily help generate specific leads for investigators (sample sizes for first digit tests often impractical to manage). Tests for conformity to Benford's Law are often described as indicating the *possibility* of the presence of fraud.
+
+Worth nothing that if, for example, certain accounts data is expected to conform to Benford’s law but doesn't, it doesn’t necessarily mean the data is fraudulent. It could however offer reason for further investigation.  
+  
+### Prerequisites
+
+The script is written in Python 2.7.
+
+Requires the following Python modules installed too if you don't already have them:
+
+```
+import pandas as pd
+import numpy as np
+```
+
+### Installing
+
+Required modules are available from PyPI and can easily be installed as follows:
+
+```
+pip install numpy
+pip install pandas
+```
+
+Note: these are not minor installs! Read more about [NumPy](http://www.numpy.org/) and [Pandas](https://pandas.pydata.org/). 
+
+## Running the Script
+
+Usage Example: 
+
+```>>> python benford.py 1 ./data/fibonacci.csv```
+
+The first parameter denotes the type of Benford test to be performed. Options are:
+
+* '1' = First digit distribution test
+* '2' = Second digit distribution test
+* '3' = Third digit distribution test
+* '12' = First two digits joint distribution test
+* '123' = First three digits joint distribution test
+
+The second parameter is the file path to the user's dataset.
+
+The script's file handling is a bit rudimentary. It reads csv text file formatted as a one-column data file, with no header row.
+
+## Other Usage Information
+
+It is left to the user to control and normalise the dataset that the script will run on. For exmaple, the user should control for negative numbers in the dataset prior to running the script. This will most likely be a common pre-processing task for accountancy data.  
+
+## Test Data
+
+I've included some test datasets in the ```/data``` repo, which include: 
+
+* The first 500 Fibonacci numbers - [data source](http://home.hiwaay.net/~jalison/Fib500.html). An almost perfect fit for the Benford Distribution. 
+
+First Digit Distribution                      | First Two Digits Joint Distribution
+:--------------------------------------------:|:-----------------------------------------------:
+![Fibonacci Distribution](/doc/fibonacci.png) | ![Fibonacci Distribution](/doc/fibonacci_12.png)
+
+* World population by country in 2017 - [data source](https://data.worldbank.org/indicator/SP.POP.TOTL?end=2017&start=1960). A statistically significant fit for the Benford Distribution.
+
+First Digit Distribution                      | First Two Digits Joint Distribution
+:--------------------------------------------:|:-----------------------------------------------:
+![World Population](/doc/world_pop_2017.png)  | ![World Population](/doc/world_pop_2017_12.png)
+
+* Monthly returns (Dec 1990 - May 2005) from Fairfield Sentry Ltd (a former Bernie Madoff fund) - [data source](https://www.sec.gov/news/studies/2009/oig-509/exhibit-0293.pdf). Visible deviations from  expected Benford distributions, but notably *not* statistically significant under Chi-Squared test. Read more hear about whether Madoff may have evaded detection via Benford analysis [read more here](). 
+
+First Digit Distribution                             | First Two Digits Joint Distribution
+:---------------------------------------------------:|:------------------------------------------------------:
+![Fairfield Sentry Monthly Returns](/doc/madoff.png) | ![Fairfield Sentry Monthly Returns](/doc/madoff_12.png)
+
+* China 2010 Census, urban area populations - [data source](https://en.wikipedia.org/wiki/Sixth_National_Population_Census_of_the_People%27s_Republic_of_China) (data download from the CN.zip file and cleaned). The data is not a fit for the first digit Benford distribution under the Chi-Squared test but does fit other digit distributions. 
+
+First Digit Distribution                             | First Two Digits Joint Distribution
+:---------------------------------------------------:|:-------------------------------------------------------:
+![Urban Populations in China](/doc/china_cities.png) | ![Urban Populations in China](/doc/china_cities_12.png)
+
+Plenty more examples at the fantastic [testingbenford.com](http://testingbenfordslaw.com)
+
+## To Do
+
+* Implement last two digits test (detect artificial rounding)
+* Add error handling
+* Improve input file handling and reading data from multiple file formats
+* Sampling issues with Chi-Squared can make it less than ideal. Implement alternative test statistics (e.g. Z tests, MAD, Komologrov-Smirnov etc...)
+
+## Built With
+
+* [Python](http://www.python.org)
+* [NumPy](http://www.numpy.org/)
+* [Pandas](https://pandas.pydata.org/)
+
+## Authors
+
+* **Andrew Houlbrook** - *Initial work* - [andrewhoulbrook](https://github.com/andrewhoulbrook)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ benford.py
@@ -0,0 +1,163 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+# A script applying Benford's Law to the investigation of datasets for potential of fraud.
+# Usage: e.g. >>> 'python benford.py 1 mydata.csv'
+#                 '1', '2', '3' can be passed to perform a fit to the first, second and third digit Benford distributions
+#                 '12' or '123' can be passed to perform a fit to the first two or first three digits Benford joint distributions 
+# Note: reads UTF-8 encoded csv file as input 
+import sys
+import math
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Read single column csv text file into a dataframe object. Return dataframe 
+def load_data(filename):
+        return pd.DataFrame(np.genfromtxt(filename, dtype=str)[0:], columns=['data'])
+
+# Count of the first, second or third digits in a list of numerical data. Return dataframe of counts
+def get_digit_counts(data, order):
+        count_data = pd.DataFrame()
+
+        # Clean input and strip out all non-numerical chars. Select significant (non-zero) digits, assign to seperate col in dataframe
+        data['digits'] = ((data['data'].replace(to_replace='[^0-9]+', value='', regex=True)).replace(to_replace='(^0+)', value='', regex=True))
+
+        # Select the first, second, third, first two or three digit combinations for each datapoint in the dataset
+        if len(order) == 2:
+                data = data.loc[data['digits'].str.len() > 1]
+                data['digits'] = data['digits'].str[0:len(order)]
+                count_data['digits'] = range(10, 100)
+        elif len(order) == 3:
+                data = data.loc[data['digits'].str.len() > 2]
+                data['digits'] = data['digits'].str[0:len(order)]
+                count_data['digits'] = range(100, 1000)
+        elif len(order) == 1:
+                data['digits'] = data['digits'].str[int(order)-1]
+                if order == '1': count_data['digits'] = range(0, 10)
+                else: count_data['digits'] = range(0, 10)
+
+        # Convert string values to int/float data types
+        data['digits'] = pd.to_numeric(data['digits'])
+
+        # Calculate count data for selected digits or digit combinations
+        counts = data.groupby(['digits']).size().reset_index(name='digit_count')
+        count_data = pd.merge(count_data, counts, on='digits', how='outer')
+
+        return count_data
+
+# Generate values for Benford distributions. Single-digit distributions as well as first two and first three digit joint distributions. Return dataframe of probabilities.
+def benford_function(order):
+    if order == '12':
+        # Calculate Benford values for first two significant digits: P(d1,d2) = log10( 1 + ( 1 / ( 10*d1 + d2 ) ) )
+        benford_values = [math.log10(1 + (1 / (10*float(str(d)[0]) + float(str(d)[1]) ))) for d in range(10, 100)]
+        benford = pd.DataFrame(data = { 'digits': range(10, 100), 'benford' : benford_values })   
+            
+    elif order == '123':
+        # Calculate Benford values for first three significant digit: P(d1,d2,d3) = log10( 1 + ( 1 / ( 100*d1 + 10*d2 + d3 ) ) )
+        benford_values = [math.log10(1 + (1 / (100*float(str(d)[0]) + 10*float(str(d)[1]) + float(str(d)[2]) ))) for d in range(100, 1000)]
+        benford = pd.DataFrame(data = { 'digits': range(100, 1000), 'benford' : benford_values })  
+
+    elif order == '1':
+        # Calculate Benford values for first significant digit: P(d1) = log10( 1 + ( 1 / d1 ) )
+        benford_values = [math.log10(1 + (1 / float(d1))) for d1 in range(1, 10)]
+        benford = pd.DataFrame(data = { 'digits': range(0, 10), 'benford' : [None] + benford_values })                                               
+
+    elif order == '2': 
+        # Calculate Benford values for second significant digit: P(d2) = Simga_d1 { log10( 1 + ( 1 / ( 10*d1 + d2 ) ) ) }
+        benford_values = [0] * 10
+        for d1 in range(1, 10): benford_values = [x + y for x, y in zip(benford_values, [math.log10(1 + (1 / (10*float(d1) + float(d2) ))) for d2 in range(0, 10)])]
+        benford = pd.DataFrame(data = { 'digits': range(0, 10), 'benford' : benford_values })    
+
+    elif order == '3':
+        # Calculate Benford values for third significant digit: P(d3) = Simga_d1_d2 { log10( 1 + ( 1 / ( 100*d1 + 10*d2 + d3 ) ) ) }
+        benford_values = [0] * 10
+        for d1 in range(1, 10): 
+            for d2 in range(0, 10):
+                benford_values = [x + y for x, y in zip(benford_values, [math.log10(1 + (1 / (100*float(d1) + 10*float(d2) + float(d3) ))) for d3 in range(0, 10)])]
+        benford = pd.DataFrame(data = { 'digits': range(0, 10), 'benford' : benford_values })      
+
+    else: return None
+        
+    return benford
+
+# Return list of expected counts under Benford's law. Return dataframe of expected counts
+def get_expected_counts(total, order):
+        # Calculate the expected counts under Benford's law using dataframes of Benford probabilities    
+        expected = benford_function(order)
+        expected['digit_count'] = expected['benford'] * total 
+
+        return expected
+
+# Calculate Chi-squared function. Return test statistic
+def get_chi2_stat(data, expected):
+        chi2_stat = (((data['digit_count'] - expected['digit_count'])**2) / expected['digit_count']).sum().sum()
+
+        return chi2_stat
+
+def main():
+        # Load dataset from filepath passed by user
+        filename = sys.argv[2].strip()
+        data_df = load_data(filename)
+        order = sys.argv[1]
+
+        # Set variables for chart display labels
+        if order == '1': chart_label = "First"
+        elif order == '2': chart_label = "Second"
+        elif order == '3': chart_label = "Third"
+        elif order == '12': chart_label = "First Two"
+        elif order == '123': chart_label = "First Three"
+        else: print "Error: select a digit distribution to test against."; sys.exit(1)
+
+        temp = benford_function(order)
+
+        # Calculate the digit counts in the user's dataset
+        count_data_df = get_digit_counts(data_df, order)
+
+        # Calculate the counts expected under Benford's law
+        count_expected_df = get_expected_counts(count_data_df['digit_count'].sum(), order)
+
+        # Print observed and expected counts to the CLI
+        print("observed counts = {}\n".format(list(count_data_df['digit_count'].values)))
+        print("expected counts = {}\n".format(list(count_expected_df['digit_count'].round(2).values)))
+
+        # Calculate Chi-Squared value
+        chi2 = round(get_chi2_stat(count_data_df.astype('float64'), count_expected_df.astype('float64')), 2)
+        
+        # Degrees of freedom and Chi-Squared critical values (at P=0.05) for significance testing
+        c_values = { '1': [8, 15.51], '2': [9, 16.92], '3': [9, 16.92], '12': [89, 111.02], '123': [899, 969.86] }
+
+        # Perform Chi-Squared significance test
+        if chi2 < c_values[order][1]: signf = True 
+        else: signf = False
+        
+        # Print Chi-Squared significance result to CLI
+        if signf: print("Observed distribution matches expected distribution.")
+        else: print("Observed distribution does not match expected.")
+
+        # Create chart object to display observed and expected counts
+        fig, ax1 = plt.subplots()
+        ax2 = ax1.twiny()
+
+        # Add observed count data to chart with chart formatting
+        count_data_df.plot(x='digits', y='digit_count', kind='bar', color='LightBlue', ax=ax1, legend=None)
+
+        # Overlay expected count data to the chart
+        count_expected_df.plot(x='digits', y='digit_count', kind='line', color='DarkGreen', ax=ax2, legend=None)
+
+        # Add title and labels to the chart with chart formatting
+        ax1.set_title('{} - Benford\'s Distribution of {} Digits'.format(filename, chart_label))
+        ax1.set_ylabel('Count - {} Digits'.format(chart_label))
+        ax1.set_xlabel('Digits')
+        plt.xlim(count_expected_df['digits'].min(), count_expected_df['digits'].max())
+        ax2.get_xaxis().set_visible(False)
+        ax2.get_yaxis().set_visible(False)
+
+        # Add Chi-Squared value and significance test information as chart annotation
+        plt.annotate(xy=[count_data_df['digits'].max()*0.7,count_data_df['digit_count'].max()*0.8], s="dof={}\nChi2={}\nCritical Value={}\nalpha=0.05".format(c_values[order][0], chi2, c_values[order][1]))
+
+        # Display chart
+        plt.show()
+
+if __name__ == '__main__':
+        main()
